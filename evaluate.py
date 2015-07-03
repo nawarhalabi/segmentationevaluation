9c64396625b2d4acde80353554d1fe2d982d44cf
Nawar Halabi <nawar.halabi@gmail.com>
2015-07-03 19:06:33 +0100

    fixed bugs

--- evaluate.py
+++ evaluate.py
@@ -7,27 +7,6 @@ import re
 import glob, os
 import math
 
-phones = ["AH", "r", "g", "y", "u0", "i1",
-"b", "z", "f", "v", "i0", "uu1",
-"t", "s", "q", "p", "AA", "ii1",
-"SH", "k", "G", "UU0", "U1",
-"j", "S", "l",  "d", "J", "II0", "I1",
-"H", "D", "m", "aa", "A", "UU1",
-"x", "T", "n", "uu0", "U0", "II1",
-"d", "Z", "h", "ii0", "I0", "sil", 
-"TH", "E", "w", "a", "u1", "sp", "^",
-
-"AHAH", "rr", "gg", "yy",
-"bb", "zz", "ff", "vv",
-"tt", "ss", "qq", "pp",
-"SHSH", "kk", "GG",
-"jj", "SS", "ll",  "dd", "JJ",
-"HH", "DD", "mm",
-"xx", "TT", "nn",
-"dd", "ZZ", "hh", 
-"THTH", "EE", "ww", "^^"
-]
-
 consonants = ["AH", "r", "g", "y",
 "b", "z", "f", "v",
 "t", "s", "q", "p",
@@ -47,22 +26,30 @@ consonants = ["AH", "r", "g", "y",
 "dd", "ZZ", "hh", 
 "THTH", "EE", "ww", "^^"
 ]
-
 vowels = ["aa", "A", "UU1", "II1",
 "uu0", "U0", "II0", "I1",
 "ii0", "I0",  "UU0", "U1",
 "a", "u1", "AA", "ii1",
-"i0", "uu1", "u0", "i1"]
-
-stops = ["AH", "b", "t", "T", "b", "p", "d", "D", "k" , "q", "J", "G",
-"AHAH", "bb", "tt", "TT", "bb", "pp", "dd", "DD", "kk" , "qq", "JJ", "GG"
+"i0", "uu1", "u0", "i1"
 ]
+pause = ["sil", "sp"
+]
+phones = consonants + vowels
+allSymbols = phones + pause
+stopsVoiced = ["b", "d", "D", "G", "J",
+"bb", "dd", "DD", "GG", "JJ"
+]
+stopsVoiceless = ["p", "t", "T", "AH", "k", "q",
+"pp", "tt", "TT", "AHAH", "kk", "qq"
+]
+stops = stopsVoiced + stopsVoiceless
 fricVoiced = ["v", "TH", "z", "Z", "j", "g", "E",
 "vv", "THTH", "zz", "ZZ", "jj", "gg", "EE"
 ]
 fricVoiceless = ["f", "S", "s", "^", "SH", "x", "H", "h",
 "ff", "SS", "ss", "^^", "SHSH", "xx", "HH", "hh"
 ]
+fric = fricVoiced + fricVoiceless
 nasals = ["m", "n",
 "mm", "nn"
 ]
@@ -72,9 +59,8 @@ trill = ["r",
 approx = ["w", "y", "l",
 "ww", "yy", "ll"
 ]
-pause = ["sil", "sp"]
 
-types = {"phones": phones, "consonants" : consonants, "vowels": vowels, "stops": stops, "fricVoiced": fricVoiced, "fricVoiceless": fricVoiceless, "nasals": nasals, "trill": trill, "approx": approx, "pause": pause}
+types = {"phones": phones, "consonants" : consonants, "vowels": vowels, "stops": stops, "stopsVoiced": stopsVoiced, "stopsVoiceless": stopsVoiceless, "fric": fric, "fricVoiced": fricVoiced, "fricVoiceless": fricVoiceless, "nasals": nasals, "trill": trill, "approx": approx, "pause": pause}
 
 orgDir = sys.argv[1] #Directory if original segmentations (either manually or automatically)
 corDir = sys.argv[2] #Directory of manually corrected segmentations
@@ -108,7 +94,7 @@ for file in glob.glob(os.path.join(orgDir, '*.TextGrid')): #Read Original bounda
 		line = line.split('=')
 		if(len(line) == 2 and line[0] == 'xmax'):
 			xmax = float(line[1])
-		if(len(line) == 2 and line[0] == 'text' and line[1] in phones and xmax != None):
+		if(len(line) == 2 and line[0] == 'text' and line[1] in allSymbols and xmax != None):
 			org[fileName].append((line[1].strip('"\''), xmax))
 			xmax = None
 	fileHandle.close()
@@ -132,7 +118,7 @@ for file in glob.glob(os.path.join(corDir, '*.TextGrid')): #Read corrected bound
 		line = line.split('=')
 		if(len(line) == 2 and line[0] == 'xmax'):
 			xmax = float(line[1])
-		if(len(line) == 2 and line[0] == 'text' and line[1] in phones and xmax != None):
+		if(len(line) == 2 and line[0] == 'text' and line[1] in allSymbols and xmax != None):
 			cor[fileName].append((line[1].strip('"\''), xmax))
 			xmax = None
 	fileHandle.close()
@@ -149,8 +135,8 @@ for fileName in cor: #Start calculating histograms
 		orgPos = 0
 		corPos = 0
 		while orgPos < len(orgUtt) - 1 and corPos < len(corUtt) - 1:
+			#print orgUtt[orgPos]
 			delta = orgUtt[orgPos][1] - corUtt[corPos][1]
-			total += 1
 			if(orgUtt[orgPos][0] == corUtt[corPos][0] and orgUtt[orgPos + 1][0] == corUtt[corPos + 1][0]):
 				for type1 in types: #For all boundary types, add the delta to the statistics
 					for type2 in types: #---------------------------------------------------
@@ -168,17 +154,24 @@ for fileName in cor: #Start calculating histograms
 								for i in range(index, len(histos[type1 + "-" + type2])):
 									histos[type1 + "-" + type2][i] += 1
 							else: #All boundaries greater than a certain threshold are added to the last entry of the histogram. This is determined by the length of the list (see if statement above)
-								histos[type1 + "-" + type2][-1] += 1
+								histos[type1 + "-" + type2][-1] += 1	
+				total += 1
 				orgPos += 1
 				corPos += 1
 			elif(orgUtt[orgPos + 1][0] == corUtt[corPos + 1][0]): #Skip to the next phone of both the correct and original alignments have the same phone next
 				orgPos += 1
 				corPos += 1
+				total += 1
 				skips += 1
+				print "skip corPos: " + str(corUtt[corPos][0]) + " orgPos: " + str(orgUtt[orgPos][0]) + " in " + fileName
+				print "skip corPos: " + str(corUtt[corPos + 1][0]) + " orgPos: " + str(orgUtt[orgPos + 1][0]) + " in " + fileName
 			else: #Increase corrected or the original Pointer to try and regain alignment if lost due to changes
 				if delta > 0:
 					corPos += 1
 					skips += 1
+					total += 1
+					print "skip corPos: " + str(corUtt[corPos][0]) + " orgPos: " + str(orgUtt[orgPos][0]) + " in " + fileName
+					print "skip corPos: " + str(corUtt[corPos + 1][0]) + " orgPos: " + str(orgUtt[orgPos + 1][0]) + " in " + fileName
 				else:
 					orgPos += 1
 #--------------------------------------------------------------------------------------------------------------------------------
@@ -192,7 +185,7 @@ for key in histos:
 	if sumH == 0:
 		sumH = 1
 	
-	res += ", ".join(str(x / float(sumH)) for x in histos[key]) + ", "
+	res += ", ".join(str(round(100 * x / float(sumH), 2)) for x in histos[key]) + ", "
 	if(deltas[key][1] != 0):
 		deltas[key][0] = float(deltas[key][0]) / float(deltas[key][1])
 		deltas[key][4] = (float(deltas[key][4]) / float(deltas[key][1])) - (deltas[key][0] * deltas[key][0])
